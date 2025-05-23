2d88ae6a7523a1f81bbd7fb91bb259858df5e0ec
mjfactor <emjayfactor@gmail.com>
2025-05-23 17:02:55 +0800

    password page

--- src/lib/db.ts
+++
@@ -1,9 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-
-// Check if DATABASE_URL is defined
-if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL environment variable is not defined');
-}
-
-// Create a SQL query executor for server components
-export const sql = neon(process.env.DATABASE_URL);
